d375f72c494b9e5f562900c24e513cc1d046b52a
zalbizadain <zalbidmi99@gmail.com>
2024-07-10 10:58:14 +0700

    membuat komponen kotak1 dan kotak2 dan juga menampilkan di app

---
+++ src/components/Kotak1.jsx
@@ -0,0 +1,9 @@
+function Kotak1() {
+  return (
+    <div>
+        <h2>Kotak1</h2>
+    </div>
+  )
+}
+
+export default Kotak1
---
+++ src/components/Kotak2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Kotak2() {
+  return (
+    <div>
+      <h2>Kotak2</h2>
+    </div>
+  )
+}
+
+export default Kotak2
